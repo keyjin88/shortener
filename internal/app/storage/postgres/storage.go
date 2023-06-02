95b3b12b805de957233c9f592540731a056bd275
AV <alexandervdovenkov@yandex.ru>
2023-06-02 22:59:29 +0300

    simplify method (iteration #12)

--- internal/app/storage/postgres/storage.go
+++ internal/app/storage/postgres/storage.go
@@ -2,7 +2,6 @@ package postgres
 
 import (
 	"context"
-	"database/sql"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/keyjin88/shortener/internal/app/logger"
 	"github.com/keyjin88/shortener/internal/app/storage"
@@ -39,17 +38,13 @@ func InitPgRepository(ctx context.Context, dataBaseDSN string) (*URLRepositoryPo
 
 func (r *URLRepositoryPostgres) FindByShortenedURL(shortURL string) (string, error) {
 	ctx := context.Background()
-	query := `SELECT * FROM public.shortened_url WHERE short_url = $1`
-	shortenedURL := storage.ShortenedURL{}
-	var correlationId sql.NullString
-	err := r.dbPool.QueryRow(ctx, query, shortURL).
-		Scan(&shortenedURL.ID, &shortenedURL.ShortURL, &shortenedURL.OriginalURL, &shortenedURL.CreatedAt, &shortenedURL.UpdatedAt, &correlationId)
-	shortenedURL.CorrelationID = correlationId.String
+	query := `SELECT original_url FROM public.shortened_url WHERE short_url = $1`
+	var originalURL string
+	err := r.dbPool.QueryRow(ctx, query, shortURL).Scan(&originalURL)
 	if err != nil {
 		return "", err
 	}
-
-	return shortenedURL.OriginalURL, nil
+	return originalURL, nil
 }
 
 func (r *URLRepositoryPostgres) Save(shortURL string, url string) (storage.ShortenedURL, error) {

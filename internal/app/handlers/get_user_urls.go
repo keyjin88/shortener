87c2ff307c3a2acca2f9fdbe6d22603591bd9018
AV <alexandervdovenkov@yandex.ru>
2023-06-15 09:42:39 +0300

    fix bugs

--- internal/app/handlers/get_user_urls.go
+++ internal/app/handlers/get_user_urls.go
@@ -8,10 +8,20 @@ import (
 
 func (h *Handler) GetUserURL(context RequestContext) {
 	uid := context.GetString("uid")
+	if uid == "" {
+		logger.Log.Infof("uid is empty")
+		context.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
 	originalURL, err := h.shortener.GetShortenedURLByUserID(uid)
 	if err != nil {
 		logger.Log.Infof("error while shortening url: %v", err)
 		context.JSON(http.StatusBadRequest, gin.H{"error": "Error while getting shortened url"})
+		return
+	}
+	if len(originalURL) == 0 {
+		logger.Log.Infof("urls not found")
+		context.AbortWithStatus(http.StatusNoContent)
 		return
 	}
 	context.JSON(http.StatusOK, originalURL)
